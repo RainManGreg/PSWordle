62b376c0337a3ffd7332db2d3b8c9d53edb29b41
unknown <bakegreg@umich.edu>
2022-02-01 09:43:09 -0500

    Moved dictionaries into local repo to improve performance

--- start-wordle.ps1
+++ start-wordle.ps1
@@ -92,10 +92,24 @@ Retrieves a dictionary of 5 letter words.
 An array of all 5 letter words in the dictionary
 #>
     if ($HardMode){
-        (invoke-webrequest -URI https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt).content.split("`n") | where-object {$_ -ne ""}
+        #(invoke-webrequest -URI https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt).content.split("`n") | where-object {$_ -ne ""}
+        $dictpath = join-path -Path $PSScriptRoot -ChildPath "assets" | join-path -ChildPath "dicts" | join-path -ChildPath "hardmode.txt"
+        if (test-path $dictpath){
+            get-content $dictpath
+        }
+        else {
+            (invoke-webrequest -URI https://github.com/RainManGreg/PSWordle/tree/main/assets/dicts/hardmode.txt).content.split("`n")
+        }
     }
     else{
-        (invoke-webrequest -Uri https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/a9e55d7e0c08100ce62133a1fa0d9c4f0f542f2c/wordle-answers-alphabetical.txt).content.split("`n")
+        #(invoke-webrequest -Uri https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/a9e55d7e0c08100ce62133a1fa0d9c4f0f542f2c/wordle-answers-alphabetical.txt).content.split("`n")
+        $dictpath = join-path -Path $PSScriptRoot -ChildPath "assets" | join-path -ChildPath "dicts" | join-path -ChildPath "dict.txt"
+        if (test-path $dictpath){
+            get-content $dictpath
+        }
+        else {
+            (invoke-webrequest -URI https://github.com/RainManGreg/PSWordle/tree/main/assets/dicts/dict.txt).content.split("`n")
+        }
     }
 }
 
