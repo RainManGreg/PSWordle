70e446b5c7e54dc152caf84804be1b34aa10610e
unknown <bakegreg@umich.edu>
2022-02-03 17:08:08 -0500

    Word tester to evaluate algorithms. Added algorithm options

---
+++ firstwordtester.ps1
@@ -0,0 +1,215 @@
+
+function Get-Dictionary {
+    [CmdletBinding()]
+    param(
+        [switch]$HardMode
+	)
+<#
+.DESCRIPTION
+Retrieves a dictionary of 5 letter words.
+
+.OUTPUTS
+An array of all 5 letter words in the dictionary
+#>
+    
+    if ($HardMode){
+        $filename = "hardmode.txt"
+        $url = "https://raw.githubusercontent.com/RainManGreg/PSWordle/main/assets/dicts/hardmode.txt"
+    }    
+    else{
+        $filename = "dict.txt"
+        $url = "https://raw.githubusercontent.com/RainManGreg/PSWordle/main/assets/dicts/dict.txt"
+    }
+    if ($PSScriptRoot){
+        $dictpath = join-path -Path $PSScriptRoot -ChildPath "assets" | join-path -ChildPath "dicts" | join-path -ChildPath $filename 
+    }
+    else {
+        $dictpath = ".\$filename"
+    }
+    if (test-path $dictpath){
+        $lower = get-content $dictpath -Encoding ascii | where-object {$_ -ne ""}
+        $lower | foreach-object {$_.toupper()}
+
+    }
+    else {
+        $tempdict = (join-path -path $env:temp -ChildPath $filename)
+        if (-not(test-path $tempdict)){
+            (invoke-webrequest -URI $url).content | out-file $tempdict -Encoding ascii
+        }
+        $lower = get-content $tempdict -Encoding ascii | where-object {$_ -ne ""}
+        $lower | foreach-object {$_.toupper()}
+    }
+}
+
+function Get-GuessHelpWordOrder2 {
+    #improved word scoring compared to original function. Does not count green letters towards the score anymore
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True)]  
+        [string[]]$PossibleWords
+    )
+
+    $letterdict = @{}
+    $worddict = @{}
+    
+    $lockedPositions = @()
+
+    foreach ($num in 0..4){
+        $LetterChanged = $False
+        $firstLetter = $PossibleWords[0].ToCharArray()[$num]
+        
+        write-verbose "Letter to check to see if it is in every word in position $num : $firstLetter"
+
+        foreach ($word in $PossibleWords){
+            if (-not($LetterChanged)){
+                write-verbose "Word we are checking is $word. Character we are checking is $($word.ToCharArray()[$num]) in position $num"
+                if ($word.ToCharArray()[$num] -ne $firstLetter){
+                    write-verbose "The letter in position $num changed so it is not a locked position"
+                    $LetterChanged = $TRUE
+                }
+            }
+        }
+        if (-Not($LetterChanged)){
+            write-verbose "The letter in position $num never changed so $num is a locked position."
+            $lockedPositions += $num
+        }
+    }
+    write-verbose "Locked Positions: $lockedPositions"
+
+    #build dictionary of letters/num times they occur
+    foreach ($word in $PossibleWords){
+        $allowedPositions = 0..4 | where-object {-Not($_ -in $lockedPositions)}
+        $ofs = ""
+        [string]$reducedWord = foreach ($num in $allowedPositions){
+            $word[$num]
+        }
+        write-verbose "Word after removing locked positions: $reducedWord"
+        foreach ($Letter in $reducedword.ToCharArray()){
+            #write-verbose $Letter
+            if ($letterdict.keys -contains $Letter){
+                #write-verbose "$Letter is already in the dictionary. Incrementing the count by one."
+                $letterdict["$Letter"] = $letterdict["$letter"] + 1
+            }
+            else{
+                #write-verbose "Adding $Letter to the dictionary."
+                $letterdict["$Letter"] = 1
+            }
+        }
+    }
+
+    write-verbose "Dictionary keys: $($letterdict.Keys)"
+    write-verbose "Dictionary values: $($letterdict.Values)"
+    foreach ($word in $PossibleWords){
+        $allowedPositions = 0..4 | where-object {-Not($_ -in $lockedPositions)}
+        $ofs = ""
+        [string]$reducedWord = foreach ($num in $allowedPositions){
+            $word[$num]
+        }
+        write-verbose "Word after removing locked positions: $reducedWord"
+        $score = 0
+        foreach ($Letter in $reducedword.ToCharArray()){
+            $score += $letterdict["$Letter"]
+        }
+        $worddict["$word"] = $score
+    }
+    #write-verbose "Word keys: $($worddict.keys)"
+    #write-verbose "Word values: $($worddict.values)"
+
+    $sortedByLetterPopularity = $worddict.GetEnumerator() | sort-object value -Descending | select-object -expandproperty key
+    $ofs = ", "
+    write-verbose "Best words according to letter popularity: "
+    foreach ($word in $sortedByLetterPopularity){
+        write-verbose "$word : $($worddict[$word])"
+    }
+    if ($sortedByLetterPopularity.count -ne 1){ 
+
+        $bestWordByLetterPopularity = $sortedByLetterPopularity[0]
+        $lockedPositions = @()
+
+        foreach ($num in 0..4){
+            $LetterChanged = $False
+            $firstLetter = ($bestWordByLetterPopularity)[$num]
+            
+            write-verbose "Letter to check to see if it is in every word in position $num : $firstLetter"
+
+            foreach ($word in $sortedByLetterPopularity){
+                if (-not($LetterChanged)){
+                    write-verbose "Word we are checking is $word. Character we are checking is $($word.ToCharArray()[$num]) in position $num"
+                    if ($word.ToCharArray()[$num] -ne $firstLetter){
+                        write-verbose "The letter in position $num changed so it is not a locked position"
+                        $LetterChanged = $TRUE
+                    }
+                }
+            }
+            if (-Not($LetterChanged)){
+                write-verbose "The letter in position $num never changed so $num is a locked position."
+                $lockedPositions += $num
+            }
+        }
+        write-verbose "Locked Positions: $lockedPositions"
+
+        $wordsWithoutUnnecessaryDoubles = foreach ($word in $sortedByLetterPopularity){
+            $allowedPositions = 0..4 | where-object {-Not($_ -in $lockedPositions)}
+            $ofs = ""
+            [string]$reducedWord = foreach ($num in $allowedPositions){
+                $word[$num]
+            }
+            write-verbose "Word after removing locked positions: $reducedWord"
+
+            $lettersOccurringMoreThanOnce = $reducedword.ToCharArray() | group-object | where-object {$_.count -gt 1}
+            if (-not($lettersOccurringMoreThanOnce)){
+                write-verbose "$word has no avoidable doubles so we are fine with it"
+                write-output $word
+            }
+        }
+        if ($wordsWithoutUnnecessaryDoubles){
+            
+                write-output $wordsWithoutUnnecessaryDoubles
+                $ofs = ", "
+                write-verbose "The words without unnecessary doubles in guess quality order are $wordsWithoutUnnecessaryDoubles."
+            
+        }
+        else{
+            write-output $sortedByLetterPopularity
+            write-verbose "All words have unnecessary doubles so returning original list $sortedByLetterPopularity from letter popularity."
+        }
+    }
+    else {
+        write-verbose "The only word in the list is $sortedByLetterPopularity so it must be the word"
+        write-output $sortedByLetterPopularity #only word so return it
+    }
+} 
+
+$dict = Get-Dictionary
+#dictreduced = $dict | select -first 10
+$wordorder = Get-GuessHelpWordOrder2 -PossibleWords $dict
+$i = 0
+
+$path = join-path -Path $PSScriptRoot -ChildPath "start-wordle.ps1"
+foreach ($firstword in $wordorder){
+    $j = 0
+    $i += 1
+    write-progress -activity "Simulating wordle ... [$i/$($wordorder.count)] Full runs : checking first word [$firstword]" -PercentComplete (($i / $wordorder.count)*100)
+    $allwordsoutput = foreach ($wordtoguess in $dict){
+        $j += 1
+        write-progress -Activity "Simulating wordle ... [$j/$($dict.count)] Word to Guess : [$wordtoguess]    [[$i/$($wordorder.count)]] First Word : [$firstword] " -PercentComplete (($i / $wordorder.count)*100)
+        & $path -WordToGuess $wordtoguess -Cheat -Simulation -FirstWord $firstword -allowedrounds 100 
+    }
+    $mean = $allwordsoutput | select-object -expandproperty score | measure-object -Average | select-object count, average
+    $failedwordsequences = $allwordsoutput | where-object {$_.score -gt 6} | select-object GuessedWords
+    $failedwords = foreach ($seq in $failedwordsequences){
+        write-output $seq.guessedwords[-1]
+    }
+    $firstguess = $allwordsoutput[0].GuessedWords[0]
+    $prop = @{
+        'Guess' = $firstguess;
+        'Mean' = $mean.average;
+        'FailedCount' = $dict.count - $mean.count;
+        'FailedWords' = "$failedwords"
+    }
+    $obj = new-object -typename psobject -prop $prop
+    "$($obj.Guess), $($obj.Mean), $($obj.FailedCount), $($obj.failedwords)" | out-file -Append -FilePath c:\temp\wordlesim.txt
+    write-output $obj
+}
+
+
--- start-wordle.ps1
+++ start-wordle.ps1
@@ -6,6 +6,30 @@
         [Parameter(ParameterSetName = 'Random')]
         [switch]$Random,
 
+        [Parameter(ParameterSetName = 'SuppliedWord')]
+        [string]$WordToGuess,
+
+        [Parameter(ParameterSetName = 'Random')]
+        [Parameter(ParameterSetName = 'Seed')]
+        [Parameter(ParameterSetName = 'SuppliedWord')]
+        [switch]$Cheat,
+
+        [Parameter(ParameterSetName = 'Random')]
+        [Parameter(ParameterSetName = 'Seed')]
+        [Parameter(ParameterSetName = 'SuppliedWord')]
+        [ValidateSet("Original","NoPointsForLockedLetters", "PositionScoring")]
+        [string]$Algorithm = "Original",
+
+        [Parameter(ParameterSetName = 'Random')]
+        [Parameter(ParameterSetName = 'Seed')]
+        [Parameter(ParameterSetName = 'SuppliedWord')]
+        [switch]$Simulation,
+
+        [Parameter(ParameterSetName = 'Random')]
+        [Parameter(ParameterSetName = 'Seed')]
+        [Parameter(ParameterSetName = 'SuppliedWord')]
+        [string]$FirstWord,
+
         [Parameter(ParameterSetName = 'Random')]
         [Parameter(ParameterSetName = 'Seed')]
         [Parameter(ParameterSetName = 'SuppliedWord')]
@@ -81,25 +105,7 @@
             "Yellow",
             "White"
         )]
-        [string]$LetterNotInWordColor = "darkgray",
-
-        [Parameter(ParameterSetName = 'Random')]
-        [Parameter(ParameterSetName = 'Seed')]
-        [Parameter(ParameterSetName = 'SuppliedWord')]
-        [switch]$Cheat,
-
-        [Parameter(ParameterSetName = 'Random')]
-        [Parameter(ParameterSetName = 'Seed')]
-        [Parameter(ParameterSetName = 'SuppliedWord')]
-        [switch]$Simulation,
-
-        [Parameter(ParameterSetName = 'Random')]
-        [Parameter(ParameterSetName = 'Seed')]
-        [Parameter(ParameterSetName = 'SuppliedWord')]
-        [string]$FirstWord,
-
-        [Parameter(ParameterSetName = 'SuppliedWord')]
-        [string]$WordToGuess
+        [string]$LetterNotInWordColor = "darkgray"
     )
 
 function Get-Dictionary {
@@ -835,7 +841,7 @@ function Get-GuessHelpPossibleWords {
     $PossibleWords
 }
 
-function Get-GuessHelpWordOrder {
+function Get-WordOrder_OG {
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory=$True)]  
@@ -844,6 +850,7 @@ function Get-GuessHelpWordOrder {
 
     $letterdict = @{}
     $worddict = @{}
+    
     #build dictionary of letters/num times they occur
     foreach ($word in $PossibleWords){
         foreach ($Letter in $word.ToCharArray()){
@@ -936,6 +943,330 @@ function Get-GuessHelpWordOrder {
         write-output $sortedByLetterPopularity #only word so return it
     }
 } 
+
+function Get-WordOrder_NoPointsForLockedLetters {
+    #improved word scoring compared to original function. Does not count green letters towards the score anymore
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True)]  
+        [string[]]$PossibleWords
+    )
+
+    $letterdict = @{}
+    $worddict = @{}
+    
+    $lockedPositions = @()
+
+    foreach ($num in 0..4){
+        $LetterChanged = $False
+        $firstLetter = $PossibleWords[0].ToCharArray()[$num]
+        
+        write-verbose "Letter to check to see if it is in every word in position $num : $firstLetter"
+
+        foreach ($word in $PossibleWords){
+            if (-not($LetterChanged)){
+                write-verbose "Word we are checking is $word. Character we are checking is $($word.ToCharArray()[$num]) in position $num"
+                if ($word.ToCharArray()[$num] -ne $firstLetter){
+                    write-verbose "The letter in position $num changed so it is not a locked position"
+                    $LetterChanged = $TRUE
+                }
+            }
+        }
+        if (-Not($LetterChanged)){
+            write-verbose "The letter in position $num never changed so $num is a locked position."
+            $lockedPositions += $num
+        }
+    }
+    write-verbose "Locked Positions: $lockedPositions"
+
+    #build dictionary of letters/num times they occur
+    foreach ($word in $PossibleWords){
+        $allowedPositions = 0..4 | where-object {-Not($_ -in $lockedPositions)}
+        $ofs = ""
+        [string]$reducedWord = foreach ($num in $allowedPositions){
+            $word[$num]
+        }
+        write-verbose "Word after removing locked positions: $reducedWord"
+        foreach ($Letter in $reducedword.ToCharArray()){
+            #write-verbose $Letter
+            if ($letterdict.keys -contains $Letter){
+                #write-verbose "$Letter is already in the dictionary. Incrementing the count by one."
+                $letterdict["$Letter"] = $letterdict["$letter"] + 1
+            }
+            else{
+                #write-verbose "Adding $Letter to the dictionary."
+                $letterdict["$Letter"] = 1
+            }
+        }
+    }
+
+    write-verbose "Dictionary keys: $($letterdict.Keys)"
+    write-verbose "Dictionary values: $($letterdict.Values)"
+    foreach ($word in $PossibleWords){
+        $allowedPositions = 0..4 | where-object {-Not($_ -in $lockedPositions)}
+        $ofs = ""
+        [string]$reducedWord = foreach ($num in $allowedPositions){
+            $word[$num]
+        }
+        write-verbose "Word after removing locked positions: $reducedWord"
+        $score = 0
+        foreach ($Letter in $reducedword.ToCharArray()){
+            $score += $letterdict["$Letter"]
+        }
+        $worddict["$word"] = $score
+    }
+    #write-verbose "Word keys: $($worddict.keys)"
+    #write-verbose "Word values: $($worddict.values)"
+
+    $sortedByLetterPopularity = $worddict.GetEnumerator() | sort-object value -Descending | select-object -expandproperty key
+    $ofs = ", "
+    write-verbose "Best words according to letter popularity: "
+    foreach ($word in $sortedByLetterPopularity){
+        write-verbose "$word : $($worddict[$word])"
+    }
+    if ($sortedByLetterPopularity.count -ne 1){ 
+
+        $bestWordByLetterPopularity = $sortedByLetterPopularity[0]
+        $lockedPositions = @()
+
+        foreach ($num in 0..4){
+            $LetterChanged = $False
+            $firstLetter = ($bestWordByLetterPopularity)[$num]
+            
+            write-verbose "Letter to check to see if it is in every word in position $num : $firstLetter"
+
+            foreach ($word in $sortedByLetterPopularity){
+                if (-not($LetterChanged)){
+                    write-verbose "Word we are checking is $word. Character we are checking is $($word.ToCharArray()[$num]) in position $num"
+                    if ($word.ToCharArray()[$num] -ne $firstLetter){
+                        write-verbose "The letter in position $num changed so it is not a locked position"
+                        $LetterChanged = $TRUE
+                    }
+                }
+            }
+            if (-Not($LetterChanged)){
+                write-verbose "The letter in position $num never changed so $num is a locked position."
+                $lockedPositions += $num
+            }
+        }
+        write-verbose "Locked Positions: $lockedPositions"
+
+        $wordsWithoutUnnecessaryDoubles = foreach ($word in $sortedByLetterPopularity){
+            $allowedPositions = 0..4 | where-object {-Not($_ -in $lockedPositions)}
+            $ofs = ""
+            [string]$reducedWord = foreach ($num in $allowedPositions){
+                $word[$num]
+            }
+            write-verbose "Word after removing locked positions: $reducedWord"
+
+            $lettersOccurringMoreThanOnce = $reducedword.ToCharArray() | group-object | where-object {$_.count -gt 1}
+            if (-not($lettersOccurringMoreThanOnce)){
+                write-verbose "$word has no avoidable doubles so we are fine with it"
+                write-output $word
+            }
+        }
+        if ($wordsWithoutUnnecessaryDoubles){
+            
+                write-output $wordsWithoutUnnecessaryDoubles
+                $ofs = ", "
+                write-verbose "The words without unnecessary doubles in guess quality order are $wordsWithoutUnnecessaryDoubles."
+            
+        }
+        else{
+            write-output $sortedByLetterPopularity
+            write-verbose "All words have unnecessary doubles so returning original list $sortedByLetterPopularity from letter popularity."
+        }
+    }
+    else {
+        write-verbose "The only word in the list is $sortedByLetterPopularity so it must be the word"
+        write-output $sortedByLetterPopularity #only word so return it
+    }
+} 
+
+function Get-WordOrder_PositionScoring {
+#figure out how to use the most common letter locations. ie look at all the possible words and choose one that has letters in the commonly found spots
+#improved word scoring compared to original function. Does not count green letters towards the score anymore
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True)]  
+        [string[]]$PossibleWords
+    )
+
+    $letterdict0 = @{}
+    $letterdict1 = @{}
+    $letterdict2 = @{}
+    $letterdict3 = @{}
+    $letterdict4 = @{}
+    $worddict = @{}
+
+    #build dictionary of letters/num times they occur
+    foreach ($word in $PossibleWords){
+        $count = 0
+        #write-verbose "Checking score of $word"
+        foreach ($Letter in $word.ToCharArray()){
+            #write-verbose $Letter
+            if ($count -eq 0){
+                if ($letterdict0.keys -contains $Letter){
+                    #write-verbose "$Letter is already in the dictionary. Incrementing the count by one."
+                    $letterdict0["$Letter"] = $letterdict0["$letter"] + 1
+                }
+                else{
+                    #write-verbose "Adding $Letter to the dictionary."
+                    $letterdict0["$Letter"] = 1
+                }
+            }
+            if ($count -eq 1){
+                if ($letterdict1.keys -contains $Letter){
+                    #write-verbose "$Letter is already in the dictionary. Incrementing the count by one."
+                    $letterdict1["$Letter"] = $letterdict1["$letter"] + 1
+                }
+                else{
+                    #write-verbose "Adding $Letter to the dictionary."
+                    $letterdict1["$Letter"] = 1
+                }
+            }
+            if ($count -eq 2){
+                if ($letterdict2.keys -contains $Letter){
+                    #write-verbose "$Letter is already in the dictionary. Incrementing the count by one."
+                    $letterdict2["$Letter"] = $letterdict2["$letter"] + 1
+                }
+                else{
+                    #write-verbose "Adding $Letter to the dictionary."
+                    $letterdict2["$Letter"] = 1
+                }
+            }
+            if ($count -eq 3){
+                if ($letterdict3.keys -contains $Letter){
+                    #write-verbose "$Letter is already in the dictionary. Incrementing the count by one."
+                    $letterdict3["$Letter"] = $letterdict3["$letter"] + 1
+                }
+                else{
+                    #write-verbose "Adding $Letter to the dictionary."
+                    $letterdict3["$Letter"] = 1
+                }
+            }
+            if ($count -eq 4){
+                if ($letterdict4.keys -contains $Letter){
+                    #write-verbose "$Letter is already in the dictionary. Incrementing the count by one."
+                    $letterdict4["$Letter"] = $letterdict4["$letter"] + 1
+                }
+                else{
+                    #write-verbose "Adding $Letter to the dictionary."
+                    $letterdict4["$Letter"] = 1
+                }
+            }
+            $count += 1
+        }
+    }
+
+    foreach ($letter in $letterdict0.keys){
+        write-verbose "Position 0 : $letter : $($letterdict0[$letter])"
+    }
+    foreach ($letter in $letterdict1.keys){
+        write-verbose "Position 1 : $letter : $($letterdict1[$letter])"
+    }
+    foreach ($letter in $letterdict2.keys){
+        write-verbose "Position 2 : $letter : $($letterdict2[$letter])"
+    }
+    foreach ($letter in $letterdict3.keys){
+        write-verbose "Position 3 : $letter : $($letterdict3[$letter])"
+    }
+    foreach ($letter in $letterdict4.keys){
+        write-verbose "Position 4 : $letter : $($letterdict4[$letter])"
+    }
+
+    #write-verbose "Dictionary keys: $($letterdict.Keys)"
+    #write-verbose "Dictionary values: $($letterdict.Values)"
+    foreach ($word in $PossibleWords){
+        $count = 0
+        $score = 0
+        foreach ($Letter in $word.ToCharArray()){
+            if ($count -eq 0){
+                $score += $letterdict0["$Letter"]
+            }
+            if ($count -eq 1){
+                $score += $letterdict1["$Letter"]
+            }
+            if ($count -eq 2){
+                $score += $letterdict2["$Letter"]
+            }
+            if ($count -eq 3){
+                $score += $letterdict3["$Letter"]
+            }
+            if ($count -eq 4){
+                $score += $letterdict4["$Letter"]
+            }
+        }
+        $worddict["$word"] = $score
+    }
+    #write-verbose "Word keys: $($worddict.keys)"
+    #write-verbose "Word values: $($worddict.values)"
+
+    $sortedByLetterPopularity = $worddict.GetEnumerator() | sort-object value -Descending | select-object -expandproperty key
+    $ofs = ", "
+    write-verbose "Best words according to letter popularity: "
+    foreach ($word in $sortedByLetterPopularity){
+        write-verbose "$word : $($worddict[$word])"
+    }
+    if ($sortedByLetterPopularity.count -ne 1){ 
+
+        $bestWordByLetterPopularity = $sortedByLetterPopularity[0]
+        $lockedPositions = @()
+
+        foreach ($num in 0..4){
+            $LetterChanged = $False
+            $firstLetter = ($bestWordByLetterPopularity)[$num]
+            
+            write-verbose "Letter to check to see if it is in every word in position $num : $firstLetter"
+
+            foreach ($word in $sortedByLetterPopularity){
+                if (-not($LetterChanged)){
+                    write-verbose "Word we are checking is $word. Character we are checking is $($word.ToCharArray()[$num]) in position $num"
+                    if ($word.ToCharArray()[$num] -ne $firstLetter){
+                        write-verbose "The letter in position $num changed so it is not a locked position"
+                        $LetterChanged = $TRUE
+                    }
+                }
+            }
+            if (-Not($LetterChanged)){
+                write-verbose "The letter in position $num never changed so $num is a locked position."
+                $lockedPositions += $num
+            }
+        }
+        write-verbose "Locked Positions: $lockedPositions"
+
+        $wordsWithoutUnnecessaryDoubles = foreach ($word in $sortedByLetterPopularity){
+            $allowedPositions = 0..4 | where-object {-Not($_ -in $lockedPositions)}
+            $ofs = ""
+            [string]$reducedWord = foreach ($num in $allowedPositions){
+                $word[$num]
+            }
+            write-verbose "Word after removing locked positions: $reducedWord"
+
+            $lettersOccurringMoreThanOnce = $reducedword.ToCharArray() | group-object | where-object {$_.count -gt 1}
+            if (-not($lettersOccurringMoreThanOnce)){
+                write-verbose "$word has no avoidable doubles so we are fine with it"
+                write-output $word
+            }
+        }
+        if ($wordsWithoutUnnecessaryDoubles){
+            
+                write-output $wordsWithoutUnnecessaryDoubles
+                $ofs = ", "
+                write-verbose "The words without unnecessary doubles in guess quality order are $wordsWithoutUnnecessaryDoubles."
+            
+        }
+        else{
+            write-output $sortedByLetterPopularity
+            write-verbose "All words have unnecessary doubles so returning original list $sortedByLetterPopularity from letter popularity."
+        }
+    }
+    else {
+        write-verbose "The only word in the list is $sortedByLetterPopularity so it must be the word"
+        write-output $sortedByLetterPopularity #only word so return it
+    }
+}
+
 #Initialize game - get word
 if ($HardMode){
     $HardModeText = "Hard Mode "
@@ -1018,7 +1349,16 @@ while (($CurrentRound -lt $ALLOWEDROUNDS) -and (-not($WonGame))){
             $possiblewords = Get-GuessHelpPossibleWords -Regex $regex
             if ($possiblewords){
                 if ($possiblewords.count -gt 1){  #only try to order them if there is more than one   
-                    $suggestedGuessOrder = Get-GuessHelpWordOrder -PossibleWords $possiblewords
+                    if ($Algorithm -eq "Original"){ 
+                        $suggestedGuessOrder = Get-WordOrder_OG -PossibleWords $possiblewords
+                    }
+                    if ($Algorithm -eq "NoPointsForLockedLetters"){ 
+                        $suggestedGuessOrder = Get-WordOrder_NoPointsForLockedLetters -PossibleWords $possiblewords
+                    }
+                    if ($Algorithm -eq "PositionScoring"){ 
+                        $suggestedGuessOrder = Get-WordOrder_PositionScoring -PossibleWords $possiblewords
+                    }
+
                     if ($suggestedGuessOrder.count -gt 1){
                         $suggestedGuess = $suggestedGuessOrder[0]
                     }
